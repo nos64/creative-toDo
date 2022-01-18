38cb08864d72da14a8ecc10aa3129365bf9043b8
Mikhail Nosov <manosov1984@gmail.com>
2022-01-18 20:56:09 +0400

    update after review

--- todo/src/components/Loader/LoaderHOC.js
+++ todo/src/components/Loader/LoaderHOC.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './LoadingHOC.css';
-import './oval.svg';
+import Oval from './oval.svg';
 
 const LoaderHOC = (WrappedComponent) => {
   return class WihLoadingComponent extends React.Component {
@@ -12,30 +12,18 @@ const LoaderHOC = (WrappedComponent) => {
     }
 
     componentDidMount() {
-      setTimeout(() => this.setState({ show: true}), 3000)
+      this.timeout = setTimeout(() => this.setState({ show: true}), 3000)
+    }
+
+    commponentDidUpdate() {
+      clearTimeout(this.interval)
     }
 
     render() {
       return this.state.show 
       ? <WrappedComponent {...this.props}/> 
       : <div className="loading">Loading...
-          <svg width="100" height="100" viewBox="0 0 38 38" 
-          xmlns="http://www.w3.org/2000/svg" stroke="rgb(223, 103, 103)">
-            <g fill="none" fill-rule="evenodd">
-              <g transform="translate(1 1)" stroke-width="2">
-                <circle stroke-opacity=".5" cx="18" cy="18" r="18"/>
-                <path d="M36 18c0-9.94-8.06-18-18-18">
-                  <animateTransform
-                    attributeName="transform"
-                    type="rotate"
-                    from="0 18 18"
-                    to="360 18 18"
-                    dur="1s"
-                    repeatCount="indefinite"/>
-                </path>
-              </g>
-            </g>
-          </svg>
+          <img src={Oval} alt='Loader'/>
         </div>
     }
   }
