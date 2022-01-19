53be79719e118e69d55122ec87c04a8f0938846e
Mikhail Nosov <manosov1984@gmail.com>
2022-01-19 23:56:59 +0400

    refactor Greeting.js

--- todo/src/components/Greeting/Greeting.js
+++ todo/src/components/Greeting/Greeting.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import './Greeting.css';
 import { ThemeContext} from '../ThemeContext/ThemeContext';
-export default class Greeting extends React.Component {
-  static contextType = ThemeContext;
 
-  render() {
-    const date = new Date();
-    const hours = date.getHours();
-    let timeOfDay;
+const Greeting = () => {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
 
-    if (hours > 6 && hours < 12) {
-      timeOfDay = 'morning';
-    } else if (hours >= 12 && hours < 18) {
-      timeOfDay = 'afternoon';
+  if (hours > 6 && hours < 12) {
+    timeOfDay = 'morning';
+  } else if (hours >= 12 && hours < 18) {
+    timeOfDay = 'afternoon';
+  } else {
+    timeOfDay = 'evening';
+  }
 
-    } else {
-      timeOfDay = 'evening';
-    }
-
-    return (
-      <h1 className={`head-${this.context} head`}>
+  return (
+    <ThemeContext.Consumer>
+      {context => (
+      <h1 className={`head-${context} head`}>
         Good {timeOfDay}!
       </h1>
-    );
-  }
+      )}
+    </ThemeContext.Consumer>
+  );
 }
+
+export default Greeting;
