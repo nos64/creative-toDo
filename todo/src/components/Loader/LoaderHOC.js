a6cd35cbad171a11fa3ffd2e340a9f79177fd390
Mikhail Nosov <manosov1984@gmail.com>
2022-01-20 16:16:10 +0400

    refactor todo

--- todo/src/components/Loader/LoaderHOC.js
+++ todo/src/components/Loader/LoaderHOC.js
@@ -1,39 +1,75 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './LoadingHOC.css';
 import Oval from './oval.svg';
 
-const LoaderHOC = (WrappedComponent) => {
-  return class WithLoadingComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        show: false,
-      };
-    }
+//Исходный компонент с работающим Loader
 
-    componentDidMount() {
-      this.timeout = setTimeout(() => this.setState({ show: true}), 1000)
-    }
+// const LoaderHOC = (WrappedComponent) => {
+//   return class WithLoadingComponent extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         show: false,
+//       };
+//     }
 
-    commponentDidUpdate() {
-      clearTimeout(this.interval)
-    }
+//     componentDidMount() {
+//       this.timeout = setTimeout(() => this.setState({ show: true}), 1000)
+//     }
 
-    render() {
-      return this.state.show 
-      ? <WrappedComponent {...this.props}/> 
-      : <div className="loading">Loading...
-          <img src={Oval} alt='Loader'/>
-        </div>
-    }
-  }
+//     commponentDidUpdate() {
+//       clearTimeout(this.interval)
+//     }
+
+//     render() {
+//       return this.state.show 
+//       ? <WrappedComponent {...this.props}/> 
+//       : <div className="loading">Loading...
+//           <img src={Oval} alt='Loader'/>
+//         </div>
+//     }
+//   }
+// }
+
+
+//Компонент который не заработал
+
+// const WithLoadingComponent = (WrappedComponent) => {
+  
+//   let [show, setShow] = useState(false);
+//   const {props} = props;
+
+//   useEffect(() => {
+//     setInterval(() => setShow(show = true), 1000)
+//   })
+
+//   if (show) {
+//     return <WrappedComponent {...props}/>
+//   } else {
+//     <div className="loading">Loading...
+//       <img src={Oval} alt='Loader'/>
+//     </div>
+//   }
+  // return(
+  //   show === 2 ? <WrappedComponent {...props}/> 
+  //   : <div className="loading">Loading...
+  //     <img src={Oval} alt='Loader'/>
+  //     </div>
+  // ) 
+  
+// }
+
+
+
+//Компонент который заработал, но без Loader
+
+const WithLoadingComponent = (WrappedComponent) => props => {
+
+  return <WrappedComponent {...props}/>  
+
 }
 
-
-
-
-
-
+const LoaderHOC = (WithLoadingComponent);
 
 
 export default LoaderHOC;
