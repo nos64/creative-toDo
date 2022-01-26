afa9ad4a77a2fb96950e7e8c3e9ecee5fc769d27
Mikhail Nosov <manosov1984@gmail.com>
2022-01-27 00:23:19 +0400

    update findDuplicateTasks

--- todo/src/redux/store.js
+++ todo/src/redux/store.js
@@ -3,8 +3,6 @@ import todoReducer from './todoSlice';
 import themeReducer from './themeSlice';
 import textBtnReducer from './textBtnSlice';
 
-
-
 export default configureStore ({
   reducer: {
     tasks: todoReducer,
